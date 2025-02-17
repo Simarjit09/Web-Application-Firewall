4512e916bb45b12c77822e50dc195700e7c09193
Simarjit Kaur <simarjk9@gmail.com>
2025-02-16 18:31:37 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+/* 
+express: our web server
+body-parser: process form data and json data
+helmet: add security headers
+morgan: log http requests to help debug issues
+*/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const waf = require("./waf"); //Import WAF middleware
+
+const app = express ();
+const PORT = 3000;
+
+app.use(helmet());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(waf);
+
+//Home Route
+app.get("/", (req, res) => {
+    res.send("Web Application Firewall (WAF) is running");
+});
+
+
+// Test route (to simulate attacks)
+app.post("/test", (req, res) => {
+    console.log("✅ Received a POST request to /test"); // Debug log
+    res.send("✅ Your request passed the security check!");
+});
+
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
---
+++ waf.js
@@ -0,0 +1,24 @@
+module.exports = function (req,res, next) {
+
+    const attackPatterns = {
+        sqlInjection: /(\b(SELECT|INSERT|UPDATE|DELETE|DROP|UNION|OR|AND)\b\s*.*['"]?\s*=|--|;)/gi,
+        xss: /(<script>|<\/script>|on\w+=|javascript:|alert\()/gi
+    };
+
+    //Check all request parameters (GET, POST, etc.)
+    for (let key in req.body) {
+        if (attackPatterns.sqlInjection.test(req.body[key]) || attackPatterns.xss.test(req.body[key])){
+            console.log("Attack detected in body: ", req.body[key]);
+            return res.status(403).send("Potential Attack detected and blocked!");
+        }
+    }
+        for (let key in req.query) {
+            if (attackPatterns.sqlInjection.test(req.query[key]) || attackPatterns.xss.test(req.query[key])) {
+                console.log("🚨 Attack detected in query:", req.query[key]);
+                return res.status(403).send("⚠️ Potential attack detected and blocked!");
+            }
+        }
+        
+        next(); //Continue to  next middleware if no attack
+}
+
